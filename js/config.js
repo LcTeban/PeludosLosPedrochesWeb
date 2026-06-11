3885e126746653fd060990aaf3d182b2e32bafd9
LcTeban <nuevotebanlc@gmail.com>
2026-06-11 20:45:16 +0200

    Remove Supabase initialization console log
    Removed console log for Supabase initialization.

--- js/config.js
+++ js/config.js
@@ -5,7 +5,6 @@ const SUPABASE_URL = 'https://grknhpyouzhmhqpjjomg.supabase.co';
 const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imdya25ocHlvdXpobWhxcGpqb21nIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzY3MDQ0NTQsImV4cCI6MjA5MjI4MDQ1NH0.z2z_eP7DCj_s-JY-ewzZ7RYXGZ0TgAOKzK4HxyoOeic';
 
 const supabaseClient = window.supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
-console.log('✅ Supabase inicializado');
 
 // Variables globales
 let dogs = [];
